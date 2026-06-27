da4cce1f51f5ab821d1aa8d6658edac5b955e55d
TilDy <dain03242@gmail.com>
2026-06-27 17:05:04 +0700

    chore: clean up test and scratch scripts

--- test_ics.py
+++
@@ -1,6 +0,0 @@
-import json
-from app.services.daa.parser import parse_ics_schedule
-with open("/home/tildy/Documents/UIT-EduAdvisor-/docs/24520245_scheduled.ics", "rb") as f:
-    ics_text = f.read()
-rows = parse_ics_schedule(ics_text)
-print(json.dumps(rows, indent=2))
--- test_parser.py
+++
@@ -1,5 +0,0 @@
-import sys
-from apps.api.app.services.daa.parser import parse_class_code_info
-
-html = open("docs/fd.html").read()
-print(parse_class_code_info(html))
